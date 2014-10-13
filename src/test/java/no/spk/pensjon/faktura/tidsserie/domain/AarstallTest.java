0449a21c7f2809f7cd1cf0dd37e4e8267a54c8c6
Tarjei Skorgenes <tas@spk.no>
2014-10-13 15:14:41 +0200

    POPKORN-106: Oppretta ein ny representasjon for årstalli form av eit verdiobjekt, Aarstall.

---
+++ src/test/java/no/spk/pensjon/faktura/tidsserie/domain/AarstallTest.java
@@ -0,0 +1,23 @@
+package no.spk.pensjon.faktura.tidsserie.domain;
+
+import org.junit.Test;
+
+import static no.spk.pensjon.faktura.tidsserie.helpers.Tid.dato;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Enheitstestar for {@link no.spk.pensjon.faktura.tidsserie.domain.Aarstall}
+ *
+ * @author Tarjei Skorgenes
+ */
+public class AarstallTest {
+    @Test
+    public void skalReturnereDatoLik1JanuarIAaret() {
+        assertThat(new Aarstall(2002).atStartOfYear()).isEqualTo(dato("2002.01.01"));
+    }
+
+    @Test
+    public void skalReturnereDatoLik31DesemberIAaret() {
+        assertThat(new Aarstall(1971).atEndOfYear()).isEqualTo(dato("1971.12.31"));
+    }
+}
