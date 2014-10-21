64563a360ea1563b37fa8080a1c2e226033a059b
Tarjei Skorgenes <tas@spk.no>
2014-10-21 15:34:20 +0200

    POPKORN-109: Legger til ny klasse som inneheld hovedalgoritma for periodisering og oppbygging av nye Underlag basert på eit sett med eit eller fleire tidsperioder som skal splittast opp i underlagsperioder.

---
+++ src/main/java/no/spk/pensjon/faktura/tidsserie/domain/underlag/UnderlagFactory.java
@@ -0,0 +1,114 @@
+package no.spk.pensjon.faktura.tidsserie.domain.underlag;
+
+import no.spk.pensjon.faktura.tidsserie.domain.periodetyper.StillingsforholdPeriode;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.asList;
+import static java.util.Objects.requireNonNull;
+import static java.util.Optional.of;
+import static java.util.stream.Collectors.toList;
+import static java.util.stream.StreamSupport.stream;
+
+/**
+ * {@link UnderlagFactory} representerer algoritma
+ * og datasettet som eit periodisert {@link no.spk.pensjon.faktura.tidsserie.domain.underlag.Underlag}
+ * blir bygd opp av og frå.
+ *
+ * @author Tarjei Skorgenes
+ */
+public class UnderlagFactory {
+    private final ArrayList<StillingsforholdPeriode> perioder = new ArrayList<>();
+    private final Observasjonsperiode grenser;
+
+    /**
+     * Konstruerer ein ny instans som kan generere underlag som er avgrensa til å ligge innanfor observasjonsperioda.
+     *
+     * @param observasjonsperiode tidsperioda som avgrensasr underlagas frå og med- og til og med-datoar
+     * @throws NullPointerException dersom <code>observasjonsperiode</code> er <code>null</code>
+     */
+    public UnderlagFactory(final Observasjonsperiode observasjonsperiode) {
+        requireNonNull(observasjonsperiode, () -> "observasjonsperiode er påkrevd, men var null");
+        this.grenser = observasjonsperiode;
+    }
+
+    /**
+     * @see #addPerioder(java.util.stream.Stream)
+     */
+    public UnderlagFactory addPerioder(final StillingsforholdPeriode... perioder) {
+        return addPerioder(asList(perioder));
+    }
+
+    /**
+     * @see #addPerioder(java.util.stream.Stream)
+     */
+    public UnderlagFactory addPerioder(final Iterable<StillingsforholdPeriode> perioder) {
+        return addPerioder(stream(perioder.spliterator(), false));
+    }
+
+    /**
+     * Legger til periodene som input-data til periodiseringa som {@link #periodiser()} utfører når eit
+     * {@link no.spk.pensjon.faktura.tidsserie.domain.underlag.Underlag} blir bygd opp.
+     *
+     * @param perioder input-perioder som skal leggast til for seinare å brukast ved periodisering av og konstruksjon av nye underlag
+     * @return <code>this</code>
+     */
+    public UnderlagFactory addPerioder(Stream<StillingsforholdPeriode> perioder) {
+        perioder.collect(() -> this.perioder, ArrayList::add, ArrayList::addAll);
+        return this;
+    }
+
+    /**
+     * Konstruerer eit nytt underlag, populert med underlagsperioder mellom alle datoar der input periodene
+     * endrar tilstand.
+     *
+     * @return eit nytt underlag med underlagsperioder i kronologisk rekkefølge mellom alle endringsdatoar i input-periodene som ligg frå og med observasjonsperiodas start og slutt
+     */
+    public Underlag periodiser() {
+        final ArrayList<Underlagsperiode> nyePerioder = new ArrayList<>();
+
+        List<LocalDate> endringsdatoar = Stream.of(
+                perioder
+                        .stream()
+                        .map(StillingsforholdPeriode::fraOgMed)
+                ,
+                perioder.stream()
+                        .map(StillingsforholdPeriode::tilOgMed)
+                        .map(o -> o.orElse(grenser.tilOgMed().get()))
+                        .map(UnderlagFactory::nesteDag)
+        )
+                .flatMap(perioder -> perioder)
+                .map(d -> d.isBefore(grenser.fraOgMed()) ? grenser.fraOgMed() : d)
+                .map(d -> d.isAfter(grenser.tilOgMed().get()) ? nesteDag(grenser.tilOgMed().get()) : d)
+                .distinct()
+                .sorted(LocalDate::compareTo)
+                .collect(toList());
+
+        Optional<LocalDate> fraOgMed = Optional.empty();
+        for (final LocalDate nextDate : endringsdatoar) {
+            fraOgMed.ifPresent(dato -> {
+                nyePerioder.add(new Underlagsperiode(dato, nextDate.minusDays(1)));
+            });
+            fraOgMed = of(nextDate);
+        }
+        return new Underlag(
+                nyePerioder
+                        .stream()
+        );
+    }
+
+    /**
+     * Returnerer dagen etter <code>dato</code>.
+     *
+     * @param dato datoen som er ein dag før datoen som blir returnert
+     * @return dagen etter <code>dato</code>
+     * @see java.time.LocalDate#plusDays(long)
+     */
+    public static LocalDate nesteDag(final LocalDate dato) {
+        return dato.plusDays(1);
+    }
+}
---
+++ src/test/java/no/spk/pensjon/faktura/tidsserie/domain/underlag/UnderlagFactoryTest.java
@@ -0,0 +1,201 @@
+package no.spk.pensjon.faktura.tidsserie.domain.underlag;
+
+import no.spk.pensjon.faktura.tidsserie.domain.periodetyper.StillingsforholdPeriode;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.time.LocalDate;
+
+import static java.time.LocalDate.now;
+import static java.time.temporal.TemporalAdjusters.lastDayOfYear;
+import static java.util.Optional.empty;
+import static java.util.Optional.of;
+import static no.spk.pensjon.faktura.tidsserie.domain.Assertions.assertFraOgMed;
+import static no.spk.pensjon.faktura.tidsserie.domain.Assertions.assertTilOgMed;
+import static no.spk.pensjon.faktura.tidsserie.helpers.Tid.dato;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Enheitstestar for UnderlagFactory.
+ *
+ * @author Tarjei Skorgenes
+ */
+public class UnderlagFactoryTest {
+    @Rule
+    public final ExpectedException e = ExpectedException.none();
+
+    private Observasjonsperiode grenser;
+
+    @Before
+    public void _before() {
+        grenser = new Observasjonsperiode(dato("1970.01.01"), now().with(lastDayOfYear()));
+    }
+
+    /**
+     * Verifiserer at underlaget som kun er generert ut frå stillingsforholdperioder, får oppretta ei underlagsperiode
+     * for kvar frå og med-dato frå alle stillingsforholdperiodene.
+     */
+    @Test
+    public void skalLageUnderlagsperiodeForKvarStillingsendring() {
+        final Underlag underlag = create()
+                .addPerioder(
+                        new StillingsforholdPeriode(dato("2005.01.01"), of(dato("2011.12.31"))),
+                        new StillingsforholdPeriode(dato("2012.01.01"), of(dato("2012.06.30")))
+                ).periodiser();
+        assertThat(underlag).hasSize(2);
+
+        assertFraOgMed(underlag, 0).isEqualTo(dato("2005.01.01"));
+        assertTilOgMed(underlag, 0).isEqualTo(of(dato("2011.12.31")));
+
+        assertFraOgMed(underlag, 1).isEqualTo(dato("2012.01.01"));
+        assertTilOgMed(underlag, 1).isEqualTo(of(dato("2012.06.30")));
+    }
+
+    /**
+     * Verifiserer at viss det eksisterer fleire input-perioder med samme fra og med-dato
+     * så blir underlaget kun forsøkt splitta ein gang på den aktuelle datoen, ikkje ein gang
+     * pr fra og med-dato slik at ein endar opp med å forsøke å konstruere underlagsperioder som har fra og med-dato
+     * etter sin til og med-dato.
+     */
+    @Test
+    public void skalKunSplittePaaUnikeEndringsdatoar() {
+        LocalDate duplisertDato = dato("2001.01.01");
+        Underlag underlag = create()
+                .addPerioder(
+                        new StillingsforholdPeriode(duplisertDato, empty()),
+                        new StillingsforholdPeriode(duplisertDato, empty())
+                ).periodiser();
+        assertThat(underlag).hasSize(1);
+        assertFraOgMed(underlag, 0).isEqualTo(dato("2001.01.01"));
+        assertThat(underlag.toList().get(0).tilOgMed().get()).isGreaterThan(duplisertDato);
+    }
+
+    /**
+     * Verifiserer at underlaget blir bygd opp med underlagsperioder i kronologisk rekkefølge sjølv om periodene som
+     * blir lagt inn i underlaget blir lagt inn i ei anna rekkefølge.
+     */
+    @Test
+    public void skalByggeOppUnderlagsperiodeneIKronologiskRekkefoelgeSjoelvOmInputPeriodeneKanVereIAnnaRekkefoelge() {
+        Underlag underlag = create()
+                .addPerioder(
+                        new StillingsforholdPeriode(dato("2010.01.01"), of(dato("2012.06.30"))),
+                        new StillingsforholdPeriode(dato("2003.07.13"), of(dato("2009.12.31")))
+                )
+                .periodiser();
+        assertThat(underlag).hasSize(2);
+        assertFraOgMed(underlag, 0).isEqualTo(dato("2003.07.13"));
+        assertTilOgMed(underlag, 1).isEqualTo(of(dato("2012.06.30")));
+    }
+
+    @Test
+    public void skalByggePeriodeSomErEinDagLang() {
+        final LocalDate expected = dato("2001.01.01");
+        final Underlag underlag = create()
+                .addPerioder(
+                        new StillingsforholdPeriode(expected, of(expected))
+                )
+                .periodiser();
+        assertThat(underlag).hasSize(1);
+        assertFraOgMed(underlag, 0).isEqualTo(expected);
+        assertTilOgMed(underlag, 0).isEqualTo(of(expected));
+    }
+
+    /**
+     * Verifiserer at underlagets siste underlagsperiode blir avgrensa til siste dag i observasjonsperioda
+     * viss den kronologisk siste tidsperioda brukt for å bygge opp underlaget, er løpande.
+     */
+    @Test
+    public void skalAvslutteSisteUnderlagsperiodaPaaSisteDagIObservasjonsperiodaVissSisteTidsperiodeErLoepande() {
+        final String expected = "2004.03.31";
+        Underlag underlag = create("2004.01.01", expected)
+                .addPerioder(new StillingsforholdPeriode(dato("2004.02.29"), empty()))
+                .periodiser();
+        assertThat(underlag).hasSize(1);
+        assertTilOgMed(underlag, 0).isEqualTo(of(dato(expected)));
+    }
+
+    /**
+     * Verifiserer at underlagets siste underlagsperiode blir avgrensa til siste dag i observasjonsperioda
+     * viss den kronologisk siste tidsperioda brukt for å bygge opp underlaget, er avslutta etter observasjonsperiodas
+     * siste dag.
+     * <h6>Grafisk illustrasjon</h6>
+     * <pre>
+     *     Observasjonsperiode:      |============|
+     *     Stillingsforholdperiode:    |=================|
+     * </pre>
+     */
+    @Test
+    public void skalAvslutteSisteUnderlagsperiodaPaaSisteDagIObservasjonsperiodaVissSisteTidsperiodesErAvsluttaSeinare() {
+        final String expected = "2008.02.28";
+
+        final Underlag underlag = create("2004.01.01", expected)
+                .addPerioder(new StillingsforholdPeriode(dato("2004.02.29"), of(dato("2012.06.01"))))
+                .periodiser();
+        assertThat(underlag).hasSize(1);
+        assertTilOgMed(underlag, 0).isEqualTo(of(dato(expected)));
+    }
+
+    /**
+     * Verifiserer at underlagets første underlagsperiode blir avgrensa til første dag i observasjonsperioda
+     * viss den kronologisk første tidsperioda brukt for å bygge opp underlaget, startar før observasjonsperiodas
+     * første dag.
+     * <h6>Grafisk illustrasjon</h6>
+     * <pre>
+     *     Observasjonsperiode:               |============|
+     *     Stillingsforholdperiode:  |=================|
+     * </pre>
+     */
+    @Test
+    public void skalStartFoersteUnderlagsperiodaPaaFoersteDagIObservasjonsperiodaVissFoersteTidsperiodesStartarTidligare() {
+        final String expected = "2004.01.01";
+        final Underlag underlag = create(expected, "2008.02.28")
+                .addPerioder(new StillingsforholdPeriode(dato("2000.02.29"), of(dato("2007.06.01"))))
+                .periodiser();
+        assertThat(underlag).hasSize(1);
+        assertFraOgMed(underlag, 0).isEqualTo(dato(expected));
+    }
+
+    /**
+     * Verifiserer at underlaget endar opp med kun ei underlagsperiode dersom input periodene som overlappar
+     * observasjonsperioda har sine frå og med- og til og med-dato utanfor observasjonsperioda.
+     * <p>
+     * Hovedintensjonen her er å sikre at periodiseringa ikkje gjer nokon antagelsar om at det alltid vil eksistere
+     * datoar å splitte på innanfor observasjonsperioda dersom den har overlappande tidsperioder som input.
+     */
+    @Test
+    public void skalKonstruereUnderlagMedKunEiUnderlagsperiodeVissInputPeriodeneOverlapparMenFraOgMedOgTilOgMedDatoaneAlleLiggUtanforObservasjonsperioda() {
+        Underlag underlag = create("2001.01.01", "2001.12.31")
+                .addPerioder(new StillingsforholdPeriode(dato("2000.01.01"), of(dato("2002.01.01"))))
+                .periodiser();
+        assertThat(underlag).hasSize(1);
+        assertFraOgMed(underlag, 0).isEqualTo(dato("2001.01.01"));
+        assertTilOgMed(underlag, 0).isEqualTo(of(dato("2001.12.31")));
+    }
+
+    /**
+     * Verifiserer at eit underlagets grenser, som representerer grenseverdiane for eldste
+     * mulige frå og med-dato og yngste mulige til og med-dato, er påkrevd.
+     * <br>
+     * Intensjonen med denne begrensinga er å forenkle periodiseringa og den seinare bruken av underlaget sidan ein no
+     * kan avgrense maksimal lengde og unngå "kanskje er har, kanskje eg ikkje har" sjekkar på sluttdato til siste
+     * underlagsperiode.
+     */
+    @Test
+    public void skalKreveEiObservasjonsperiode() {
+        e.expect(NullPointerException.class);
+        e.expectMessage("observasjonsperiode er påkrevd, men var null");
+        new UnderlagFactory(null);
+    }
+
+    private UnderlagFactory create(final String fraOgMed, final String tilOgMed) {
+        return new UnderlagFactory(
+                new Observasjonsperiode(dato(fraOgMed), dato(tilOgMed))
+        );
+    }
+
+    private UnderlagFactory create() {
+        return new UnderlagFactory(grenser);
+    }
+}
