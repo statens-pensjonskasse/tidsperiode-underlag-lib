8d27eb427ab0721cde14947ed9287bb71bd1f6d8
Tarjei Skorgenes <tas@spk.no>
2014-11-04 10:28:37 +0100

    POPKORN-111: Oppretta eit nytt aggregat som samlar all medlemsspesifikk informasjon og som tar seg av konvertering og uthenting av dei forskjellige datatypene som er påkrevd for å støtte beregning av maskinelt grunnlag eller andre premierelaterte beregningar.

---
+++ src/main/java/no/spk/pensjon/faktura/tidsserie/domain/periodisering/Medlemsdata.java
@@ -0,0 +1,112 @@
+package no.spk.pensjon.faktura.tidsserie.domain.periodisering;
+
+import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Stillingsendring;
+
+import java.util.List;
+
+import static java.util.Objects.requireNonNull;
+import static java.util.stream.Collectors.toSet;
+
+/**
+ * {@link no.spk.pensjon.faktura.tidsserie.domain.periodisering.Medlemsdata} representerer all
+ * informasjon tilknytta eit bestemt medlem, som er påkrevd for å støtte premie- og lønnsberegning for medlemet.
+ * <p>
+ * Dei tre primære datatypene som er påkrevd for dette er stillingsendringar frå stillingshistorikken,
+ * medregningar og avtalekoblingar for alle stillingsforholda tilknytta medlemmet og som ein skal gjere beregningar
+ * for.
+ * <p>
+ * Den medlemsspesifikke informasjonen som objektet inneheld blir internt og eksternt handtert som ei samling med tekstverdiar,
+ * organisert som rader og kolonner i form av ei liste som inneheld lister som igjen inneheld strengar.
+ * Denne representaasjonen er valgt for å holde serialiseringa enkel og forenkle handtering av stillingshistorikk,
+ * medregning og avtalekoblingar utan å måtte lage spesialisert serialisering for kvar og ein av desse.
+ * <p>
+ * Kontrakta for korleis kvar rad må sjå ut, varierer mellom dei 3 forskjellige datatypene.
+ * <p>
+ * <h4>Avtalekobling</h4>
+ * Ei avtalekobling skal inneholde følgjande verdiar, alle representert som tekst:
+ * <table>
+ * <thead>
+ * <tr>
+ * <td>Index</td>
+ * <td>Verdi / Format</td>
+ * <td>Beskrivelse</td>
+ * </tr>
+ * </thead>
+ * <tbody>
+ * <tr>
+ * <td>0</td>
+ * <td>1</td>
+ * <td>Typeindikator som identifiserer rada som ei avtalekobling</td>
+ * </tr>
+ * <tr>
+ * <td>1</td>
+ * <td>Long</td>
+ * <td>Stillingsforholdnr</td>
+ * </tr>
+ * <tr>
+ * <td>2</td>
+ * <td>yyyyMMdd</td>
+ * <td>Fødselsdato for medlem</td>
+ * </tr>
+ * <tr>
+ * <td>3</td>
+ * <td>5-sifra tall</td>
+ * <td>Personnummer for medlem</td>
+ * </tr>
+ * <tr>
+ * <td>4</td>
+ * <td>yyyy.MM.dd</td>
+ * <td>Startdato, første dag i perioda stillingsforholdet er tilknytta avtalen</td>
+ * </tr>
+ * <tr>
+ * <td>5</td>
+ * <td>yyyy.MM.dd</td>
+ * <td>Sluttdato, siste dag i perioda stillingsforholdet er tilknytta avtalen</td>
+ * </tr>
+ * <tr>
+ * <td>6</td>
+ * <td>6-sifra tall</td>
+ * <td>Avtalenummer, avtalen stillingsforholdet er tilknytta i den aktuelle perioda</td>
+ * </tr>
+ * </tbody>
+ * </table>
+ *
+ * @author Tarjei Skorgenes
+ */
+public class Medlemsdata {
+    private final List<List<String>> endringar;
+
+    /**
+     * Konstruerer eit nytt sett med medlemsdata basert på <code>medlemsdata</code>.
+     * <p>
+     * Referansen til datasettet som blir sendt inn blir brukt direkte for å unngå allokering og kopiering til ei ny
+     * samling, endringar gjort på <code>medlemsdata</code> etter konstruksjon vil derfor vere direkte synlig for Medlemsdata.
+     *
+     * @param medlemsdata
+     * @throws java.lang.NullPointerException     viss <code>medlemsdata</code> er <code>null</code>
+     * @throws java.lang.IllegalArgumentException viss <code>medlemsdata</code> ikkje inneheld noko informasjon og er tom
+     */
+    public Medlemsdata(final List<List<String>> medlemsdata) {
+        requireNonNull(medlemsdata, () -> "medlemsdata er påkrevd, men var null");
+        if (medlemsdata.isEmpty()) {
+            throw new IllegalArgumentException(
+                    "medlemsdata må inneholde minst ei stillingsendring, medregning eller avtalekobling, men var tom"
+            );
+        }
+
+        this.endringar = medlemsdata;
+    }
+
+    /**
+     * Hentar ut alle stillingsendringar tilknytta medlemmet.
+     *
+     * @return alle stillingsendring for medlemmet
+     */
+    Iterable<Stillingsendring> alleStillingsendringar() {
+        return endringar
+                .stream()
+                .filter(e -> "0".equals(e.get(0)))
+                .map(e -> new Stillingsendring())
+                .collect(toSet());
+    }
+}
---
+++ src/test/java/no/spk/pensjon/faktura/tidsserie/domain/periodisering/MedlemsdataTest.java
@@ -0,0 +1,65 @@
+package no.spk.pensjon.faktura.tidsserie.domain.periodisering;
+
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.ExpectedException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static java.util.Arrays.asList;
+import static org.assertj.core.api.Assertions.assertThat;
+
+/**
+ * Enheitstestar for {@link no.spk.pensjon.faktura.tidsserie.domain.periodisering.Medlemsdata}.
+ *
+ * @author Tarjei Skorgenes
+ */
+public class MedlemsdataTest {
+    @Rule
+    public final ExpectedException e = ExpectedException.none();
+
+    /**
+     * Verifiserer at endringar av type 1 er dei einaste som blir forsøkt konvertert til
+     * {@link no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Stillingsendring}.
+     */
+    @Test
+    public void skalKonvertereType0TilStillingsendring() {
+        List<List<String>> endringar = asList(
+                asList(
+                        "0"
+                ),
+                asList(
+                        "0"
+                ),
+                asList(
+                        "0"
+                )
+        );
+        final Medlemsdata data = new Medlemsdata(endringar);
+        assertThat(data.alleStillingsendringar()).as("alle stillingendringar i medlemsdata " + data).hasSize(3);
+    }
+
+    /**
+     * Verifiserer at det ikkje er tillatt å opprette medlemsdata for eit medlem som vi ikkje har noko informasjon
+     * på.
+     */
+    @Test
+    public void skalIkkjeTillateMedlemsdataUtanInnhold() {
+        e.expect(IllegalArgumentException.class);
+        e.expectMessage("medlemsdata må inneholde minst ei stillingsendring, medregning eller avtalekobling, men var tom");
+
+        new Medlemsdata(new ArrayList<>());
+    }
+
+    /**
+     * Verifiserer at det ikkje er tillatt å konstruere nye medlemsdata der data-parameteret er null ettersom det vil
+     * føre til NullPointerException ved seinare prosessering.
+     */
+    @Test
+    public void skalIkkjeTillateDataLikNullVedKonstruksjon() {
+        e.expect(NullPointerException.class);
+        e.expectMessage("medlemsdata er påkrevd, men var null");
+        new Medlemsdata(null);
+    }
+}
