3f2a9ae1c87e1ca1bb8b134f7933d613ea225b23
Snorre E. Brekke <snorre.brekke@spk.no>
2015-05-15 09:48:14 +0200

    POPKORN-153 - Endret Produktinfo konstanter fra int til Produktinfo.

--- src/main/java/no/spk/pensjon/faktura/tidsserie/domain/avtaledata/Produktinfo.java
+++ src/main/java/no/spk/pensjon/faktura/tidsserie/domain/avtaledata/Produktinfo.java
@@ -6,9 +6,9 @@ import java.util.Arrays;
  * @author Snorre E. Brekke - Computas
  */
 public final class Produktinfo {
-    public static final int GRU_35  = 35;
-    public static final int GRU_36  = 36;
-    public static final int YSK_79  = 79;
+    public static final Produktinfo GRU_35  = new Produktinfo(35);
+    public static final Produktinfo GRU_36  = new Produktinfo(36);
+    public static final Produktinfo YSK_79  = new Produktinfo(79);
 
     private final int kode;
 
@@ -16,9 +16,9 @@ public final class Produktinfo {
         this.kode = kode;
     }
 
-    public static boolean erEnAv(Produktinfo produktinfoSomSjekkes, int... erEnAvKoder) {
-        return Arrays.stream(erEnAvKoder)
-                .filter(p -> p == produktinfoSomSjekkes.kode)
+    public static boolean erEnAv(Produktinfo produktinfoSomSjekkes, Produktinfo... erEnAv) {
+        return Arrays.stream(erEnAv)
+                .filter(p -> p.equals(produktinfoSomSjekkes))
                 .findFirst()
                 .isPresent();
     }
