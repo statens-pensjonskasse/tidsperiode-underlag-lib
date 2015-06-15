a4299a2eb0b597d61e21ee0d2b5f8b163e84e70f
Tarjei Skorgenes <tas@spk.no>
2015-06-15 15:35:08 +0200

    SPKMASTER-11291, POPKORN-156: Fiksar kompileringsfeil relatert til type inference som feilar på 1.8.0_11 men som fungerer på 1.8.0_20+

--- src/test/java/no/spk/pensjon/faktura/tidsserie/domain/avtaledata/Assertions.java
+++ src/test/java/no/spk/pensjon/faktura/tidsserie/domain/avtaledata/Assertions.java
@@ -4,25 +4,28 @@ import static java.util.Optional.of;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.Optional;
+import java.util.function.Function;
 
 import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Kroner;
+import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Sats;
 import no.spk.pensjon.faktura.tidsserie.domain.grunnlagsdata.Satser;
 
 import org.assertj.core.api.AbstractObjectAssert;
 
 public class Assertions {
     public static AbstractObjectAssert<?, Optional<String>> assertArbeidsgiverbeloep(final Avtaleprodukt produkt) {
-        return assertThat(of(produkt.premiesatser()).map(Satser::arbeidsgiverpremie).map(Object::toString))
+        final Optional<? extends Satser<?>> premiesatser = of(produkt.premiesatser());
+        return assertThat(premiesatser.map(satser -> satser.arbeidsgiverpremie()).map(Object::toString))
                 .as("premiebeløp for arbeidsgiver fra " + produkt);
     }
 
     public static AbstractObjectAssert<?, Optional<String>> assertMedlemsbeloep(final Avtaleprodukt produkt) {
-        return assertThat(of(produkt.premiesatser()).map(Satser::medlemspremie).map(Object::toString))
+        return assertThat(of(produkt.premiesatser()).map(satser -> satser.medlemspremie()).map(Object::toString))
                 .as("premiebeløp for medlem fra " + produkt);
     }
 
     public static AbstractObjectAssert<?, Optional<String>> assertAdministrasjonsgebyrbeloep(final Avtaleprodukt produkt) {
-        return assertThat(of(produkt.premiesatser()).map(Satser::administrasjonsgebyr).map(Object::toString))
+        return assertThat(of(produkt.premiesatser()).map(satser -> satser.administrasjonsgebyr()).map(Object::toString))
                 .as("premiebeløp for administrasjonsgebyr fra " + produkt);
     }
 }
